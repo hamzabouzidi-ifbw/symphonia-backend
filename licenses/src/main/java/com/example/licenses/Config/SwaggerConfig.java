b2aed76bae7dd2800bd13485d21e3d41ae1c6b2a
RaniaBs21 <raniabensalem53@gmail.com>
2025-04-11 16:53:11 +0200

    config swagger

---
+++ licenses/src/main/java/com/example/licenses/Config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.licenses.Config;
+
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")  // Ajustez ce chemin pour correspondre à votre API
+                .build();
+    }
+}
---
+++ licenses/src/main/java/com/example/licenses/LicensesApplication.java
@@ -0,0 +1,18 @@
+package com.example.licenses;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@OpenAPIDefinition
+@SpringBootApplication
+public class LicensesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicensesApplication.class, args);
+    }
+
+}
